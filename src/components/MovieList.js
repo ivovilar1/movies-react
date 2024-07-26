11f503fcfeeb340b817340be7eccf4563c4f7299
Ivo <ivo_vilar@hotmail.com>
2024-07-26 15:16:34 -0300

    add search

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,23 +1,26 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from 'react';
 import MovieItem from './MovieItem';
 
+const MovieList = ({ movies }) => {
+  const [popularMovies, setPopularMovies] = useState([]);
 
-const MovieList = () => {
-    const [ movies, setMovies] = useState([]);
-
-    useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
+  useEffect(() => {
+    if (!movies) {
+      fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
         .then(response => response.json())
-        .then(data => setMovies(data.results));
-    }, []);
+        .then(data => setPopularMovies(data.results));
+    }
+  }, [movies]);
 
-    return (
-        <div>
-            {movies.map(movie => (
-                <MovieItem key={movie.id} movie={movie} />
-            ))}
-        </div>
-    );
+  const movieList = movies || popularMovies;
+
+  return (
+    <div>
+      {movieList.map(movie => (
+        <MovieItem key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
 };
 
 export default MovieList;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieList from "../components/MovieList";
+import SearchBar from "../components/SearchBar";
 
 const HomePage = () => {
 
+    const [searchResults, setSearchResults] = useState([]);
+    const handleSearch = ( query ) => {
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&query=${query}`)
+        .then(response => response.json())
+        .then(data => setSearchResults(data.results));
+    };
     return (
         <div>
             <h1>Popular Movies</h1>
-            <MovieList />
+            <SearchBar onSearch={handleSearch} />
+            <MovieList movies={searchResults.length > 0 ? searchResults : null } />
         </div>
     );
 };
